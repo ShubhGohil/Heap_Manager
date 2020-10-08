56e4465516847dcfb84e2375cd7e3dd6a6b44de5
ShubhGohil <sgohil0646@gmail.com>
2020-10-08 23:43:25 +0530

    Some changes

--- Http server/readme.md
+++ Http server/readme.md
@@ -1,1 +1,2 @@
 1 - An initial commit just to start
+2 - Did some changes to take input by telnet
--- Http server/test.py
+++ Http server/test.py
@@ -8,35 +8,50 @@ socket = socket(AF_INET, SOCK_STREAM)
 socket.bind(("", serverport))
 socket.listen(50)
 
-
+h_flag=0
 def client_request(clientsocket, address):
-	#while True:
-	message = connectionsocket.recv(4096)
-	try:
+	complete_message=''
+	while True:
+		message = clientsocket.recv(4096)
 		message = message.decode('utf-8')
-		requests = message.split('\n')
-	except UnicodeDecodeError:
-		requests = message.split('\n')
-		requests = requests[0].decode(errors = 'ignore')
-	first_line = requests[0].split(' ')
+		
+		complete_message+=message
+		if message=='\r\n':
+			break
+		'''except UnicodeDecodeError:
+			requests = message.split('\n')
+			requests = requests[0].decode(errors = 'ignore')'''
+	requests = complete_message.split('\r\n\r\n')
+	header_list = requests[0].split('\r\n')
+	first_line = header_list[0].split()
 	method = first_line[0]
 	path = first_line[1]
 	version = first_line[2]
-	print(method, path, version)
+	#print(method, path, version)
 	if method == 'GET':
 		pass
-
+	
 
 while True:
 
 	connectionsocket, addr = socket.accept()
 	t1 = threading.Thread(target=client_request, args=(connectionsocket, addr))
 	t1.start()
-
-
-"""
-create a socket
-do multithreading 
-create some html trail files
-create a server log"""
-
+'''complete_message=''	
+while True:
+	message = input()
+	message=message.encode()
+	message = message.decode('utf-8')
+	print(message)
+	complete_message+=message
+	if message=='\n':
+		break
+print(complete_message)
+header_list=complete_message.split('\n')
+first_line = header_list[0].split()
+method = first_line[0]
+path = first_line[1]
+version = first_line[2]
+	
+print(method, path, version)
+'''
